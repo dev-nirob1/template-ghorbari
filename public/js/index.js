ad693f03e6406d46e9ad5334d89b492f62e574c3
dev-nirob1 <dev.nirob1@gmail.com>
2025-07-26 01:11:10 +0600

    html template

---
+++ public/js/index.js
@@ -0,0 +1,23 @@
+const navbarToggle = () => {
+  const navbar = document.querySelector(".navbar");
+  const toggleMenu = document.querySelector(".hamburger");
+  const nav_links = document.querySelector(".nav-links");
+  // navbar toggle
+  toggleMenu.addEventListener("click", () => {
+    if (nav_links.classList.contains("active")) {
+      nav_links.classList.remove("active");
+    } else {
+      nav_links.classList.add("active");
+    }
+  });
+
+  // Add scroll animation to navbar
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 10) {
+      navbar.classList.add("scrolling");
+    } else {
+      navbar.classList.remove("scrolling");
+    }
+  });
+};
+navbarToggle();
